87a3dd7525dc4e59fea65e9d46c4cd60e03c73f5
joe beuckman <joebeuckman@gmail.com>
2013-07-09 12:11:16 -0500

    fixed test and test passing with updated TiShadow compiler.js

--- spec/async_utility_spec.js
+++ spec/async_utility_spec.js
@@ -1,23 +1,24 @@
-
-
 describe("Async Utility", function() {
-	
-	it("should load data", function() {
-	    
-	    var data = '';
-	    
-	    runs(function(){
-	        require('/async_utility').getData()
-               .then(function(response){
-                   data = response;
-               });
-	    })
-	    
-	    waitsFor(function(){
-	        expect(data).not.toEqual('');
-	    }, "The data should be loaded", 2000);
 
-	});
+    it("should load data", function() {
+
+        var data = '', loaded = false;
+
+        runs(function() {
+            require('/async_utility').getData().then(function(response) {
+                data = response;
+                loaded = true;
+            });
+        })
+    
+        waitsFor(function() {
+            return loaded;
+        }, "The data should be loaded", 2000);
+
+        runs(function() {
+            expect(data).not.toEqual('');
+        });
+    });
 
 });
 
